a6a1c31e18e50c99c5f097e835bf6825e35ed987
Madeline Stark <madeline.stark1@gmail.com>
2018-09-12 15:29:54 +0000

    automatically backed up by learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -3,10 +3,10 @@
 class Puppy
   attr_accessor :name, :breed, :age
 
-  def initialize(text)
-    @name = text[:name] #i think do this b/c accessing from param
-    @breed = text[:breed]
-    @age = text[:age]
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
   end
 
 end
